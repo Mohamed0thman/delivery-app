9b1954c70b65208595e059fad1e14cdd857e1eef
Mohamed0thman <mohamed.0thman9988@gmail.com>
2021-07-29 15:53:44 +0200

    project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 var cors = require("cors");
 
 const storeRouter = require("./routers/store");
@@ -7,11 +7,6 @@ const productRouter = require("./routers/products");
 const usersRouter = require("./routers/users");
 const shoppingCartRouter = require("./routers/cart");
 const ordersRouter = require("./routers/orders");
-
-// const brandsRouter = require("./routers/brands");
-// const feedbackRouter = require("./routers/feedback");
-// const optionsRouter = require("./routers/options");
-// const galleriesRouter = require("./routers/galleries");
 
 const app = express();
 app.use(cors());
@@ -28,9 +23,5 @@ app.use(usersRouter);
 app.use(shoppingCartRouter);
 app.use(ordersRouter);
 
-// app.use(brandsRouter);
-// app.use(feedbackRouter);
-// app.use(optionsRouter);
-// app.use(galleriesRouter);
 
 module.exports = app;
